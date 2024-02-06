bdc8d2a2ffc797d3191e00239389f9c16eab7228
grafica <paolo@monema.it>
2024-02-06 12:01:53 +0100

    problema fetch dati netlify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,16 @@ import { ItemsSites } from "./generated2/models/ItemsSites";
 import { ItemsSitesStaff } from "./generated2/models/ItemsSitesStaff";
 
 async function Home() {
-  const token: any | undefined = await getToken();
+  const token = await getToken();
   console.log(token, "token");
-  const data: any | undefined = await fetchSiteData(token);
+  const data = await fetchSiteData(token);
   console.log(data, "data");
-  const dataSite: any | undefined = await data.data.find(
+  const dataSite = await data.data.find(
     (item: ItemsSites) => item.id === process.env.SITE_ID
   );
 
-  const dataAssets: { data: Files[] | undefined } = await fetchAssetsData(
-    token
-  );
-  const dataStaff: { data: ItemsSitesStaff[] | undefined } =
-    await fetchStaffData(token);
+  const dataAssets: { data: Files[] } = await fetchAssetsData(token);
+  const dataStaff: { data: ItemsSitesStaff[] } = await fetchStaffData(token);
 
   return (
     <div className={styles.main}>
